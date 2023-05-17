ee0e8b6b60c0d59f16a3da9695f360ed9144a69b
zzw <3279527604@qq.com>
2023-05-17 19:58:20 +0800

    测试

--- Snake/src/com/zzw/snake/GitTest.java
+++ Snake/src/com/zzw/snake/GitTest.java
@@ -3,5 +3,7 @@ package com.zzw.snake;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git");
+        System.out.println("hello Git");
     }
 }
